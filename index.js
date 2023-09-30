4ee56ecfc5ab1879abd455803f7374f03e37d45e
chamok barmon <chamokbarmon123@gmail.com>
2023-09-30 23:53:00 +0600

    get productDetails route

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const express = require('express');
-const app = express()
-const cors = require('cors');
-app.use(cors())
-
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
 
 const Port = 5000;
-app.get('/',(req,res)=>{
-    res.send("server is good")
-})
+app.get("/", (req, res) => {
+  res.send("server is good");
+});
 
-app.listen(Port,()=>{
-    console.log("Server is running ",{Port});
-})
+app.listen(Port, () => {
+  console.log("Server is running ", { Port });
+});
 
+const { MongoClient, ObjectId } = require("mongodb");
 
-const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://DashboardDB:Pd04y1XPCxJupl96@cluster0.ojfpjwh.mongodb.net/?retryWrites=true&w=majority";
 
+const client = new MongoClient(uri);
 
-const uri = "mongodb+srv://DashboardDB:Pd04y1XPCxJupl96@cluster0.ojfpjwh.mongodb.net/?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri)
-
-const productCollection = client.db("DashboardDb").collection("ProductsDb")
-
+const productCollection = client.db("DashboardDb").collection("ProductsDb");
 
 // const client = new MongoClient(uri, {
 //     serverApi: {
@@ -32,12 +29,9 @@ const productCollection = client.db("DashboardDb").collection("ProductsDb")
 //     }
 //   });
 
-    
-
 async function run() {
   try {
     await client.connect();
- 
   } catch (error) {
     console.log(error.name, error.message);
   }
@@ -45,42 +39,49 @@ async function run() {
 
 run();
 
-  app.post("/product", async (req, res) => {
-    console.log(req.body)
-    try {
-      const result = await productCollection.insertOne(req.body);
-       
-      if (result.insertedId){
-        res.send({
-          success: true,
-          message:`successfully created the ${req.body.name} post is created and your id ${result.insertedId} `,
-        });
-      } else {
-        res.send({
-          success: false,
-          error: "couldn't create the post",
-        });
-      }
-    } catch (error) {
-      console.log(error.name, error.message);
+app.post("/product", async (req, res) => {
+  console.log(req.body);
+  try {
+    const result = await productCollection.insertOne(req.body);
+
+    if (result.insertedId) {
+      res.send({
+        success: true,
+        message: `successfully created the ${req.body.name} post is created and your id ${result.insertedId} `,
+      });
+    } else {
       res.send({
         success: false,
-        error: error.message,
+        error: "couldn't create the post",
       });
     }
-  });
+  } catch (error) {
+    console.log(error.name, error.message);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
-app.get('/product',async(req,res)=>{
-    try {
-        const cursor = productCollection.find({})
-        const result = await cursor.toArray()
-        res.send(result)
-      
-       
-    } catch (error) {
-        console.log(error.message);
-        
-    }
-})
-
-
+app.get("/product", async (req, res) => {
+  try {
+    const cursor = productCollection.find({});
+    const result = await cursor.toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+app.get("/productDetails/:id", async (req, res) => {
+  const id = req.params.id;
+  // query data
+  const query = { _id: new ObjectId(id) };
+  try { 
+    const result = await productCollection.findOne(query);
+    res.status(200).send(result);
+    console.log(result)
+  } catch (error) {
+    console.log(error.message);
+  }
+});
